53ee51b21c04d6e8ab78ff1a71b5bbc96c1d95f1
Dedy Sujana <dedysujana@gmail.com>
2023-05-08 06:02:20 +0700

    Perbaiki kesalahan

--- lib/materi_construction.dart
+++ lib/materi_construction.dart
@@ -1,24 +1,47 @@
+import 'dart:convert';
+
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class Mahasiswa {
   String? nama;
   String? jurusan;
-  String? angkatan;
+  int? angkatan;
 
-  Mahasiswa(
-      {String? namaMahasisa,
-      String? jurusanKuliah,
-      String? angkatanTahun}) {
-    nama = namaMahasisa;
-    jurusan = jurusanKuliah;
-    angkatan = angkatanTahun;
+  Mahasiswa() {
+    print('Constructor default');
+  }
+
+  Mahasiswa.namedConst(this.nama, this.jurusan, {this.angkatan}) {
+    print('Constructor parameterized');
+  }
+
+  Mahasiswa.fromJson(Map<String, dynamic> json) {
+    nama = json['nama'];
+    jurusan = json['jurusan'];
+    angkatan = json['angkatan'];
+  }
+
+  Mahasiswa.fromJsonString(String jsonString) {
+    Map<String, dynamic> json = jsonDecode(jsonString);
+    nama = json['nama'];
+    jurusan = json['jurusan'];
+    angkatan = json['angkatan'];
   }
 }
 
 void main() {
-  Mahasiswa mahasiswa = Mahasiswa(
-      namaMahasisa: 'Dedy Sujana',
-      jurusanKuliah: 'Programming',
-      angkatanTahun: '2000');
-  print('Nama: ${mahasiswa.nama}');
-  print('Jurusan: ${mahasiswa.jurusan}');
-  print('Angkatan: ${mahasiswa.angkatan}');
+  String jsonString1 =
+      '{"nama":"Dedy Sujana", "jurusan":"Teknik Informatika","angkatan":2000}';
+  String jsonString2 =
+      '{"nama":"Ayu SUlistyowati", "jurusan":"Management","angkatan":2001}';
+
+  Mahasiswa ms1 = Mahasiswa.fromJsonString(jsonString1);
+  print('Nama: ${ms1.nama}');
+  print('Jurusan: ${ms1.jurusan}');
+  print('Angkatan: ${ms1.angkatan}');
+
+  Mahasiswa ms2 = Mahasiswa.fromJsonString(jsonString2);
+  print('Nama: ${ms2.nama}');
+  print('Jurusan: ${ms2.jurusan}');
+  print('Angkatan: ${ms2.angkatan}');
+  
 }
